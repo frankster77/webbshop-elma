75ae9a167b2900a21dd57c2d83c3e6cecc081c57
Elma Wigren <elmawigren3@gmail.com>
2026-04-28 09:49:59 +0200

    fixed products from database

---
+++ Models/Products.php
@@ -0,0 +1,19 @@
+<?php
+
+class Product
+{
+    public $id;
+    public $name;
+    public $price;
+    public $popularityFactor;
+    public $categoryId;
+    public $imgUrl;
+    public $description;
+
+   
+}
+
+class Laptop extends Product
+{
+   
+}
